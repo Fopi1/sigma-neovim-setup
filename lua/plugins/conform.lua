f8305195283cd377c92694bbd55f799c56887ee4
Fopi1 <hfh535mail.com@gmail.com>
2026-01-10 14:53:50 +0300

    da

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettierd", "prettier", stop_after_first = true },
 			htmlangular = { "prettierd", "prettier", stop_after_first = true },
 			css = { "prettierd", "prettier", stop_after_first = true },
